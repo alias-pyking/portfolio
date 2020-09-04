1862ad82fafba827bcc4be651e70ad9d38cd78eb
shubhamsks <shubhamkumar7051@gmail.com>
2020-09-05 00:03:34 +0530

    added filters code

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -32,4 +32,38 @@
 // 	contactElement.classList.add('active');
 // 	filters.style.display = 'none';
 // }
-// // applying filters
+
+// sidenavigation responsive functionality
+
+const menuButton = document.getElementById('menu-btn');
+
+menuButton.addEventListener('click', (event) => {
+	const sideBar = document.getElementById('sidebar');
+	sideBar.style.width = "50vw";
+	sideBar.style.display = "block";
+	sideBar.style.opacity = "1";
+	sideBar.style.background = "#fdfdfd";
+	sideBar.style.zIndex = "4";
+	let links = sideBar.getElementsByTagName('a');
+	let pEls = sideBar.getElementsByTagName('p');
+	let elements = [...links, ...pEls];
+	elements = Array.from(elements);
+	elements.forEach(elem => {
+		elem.style.display = 'block';
+	});
+	document.querySelector('.profile').style.display = 'block';
+});
+
+function closeNav() {
+	const sideBar = document.getElementById('sidebar');
+	sideBar.style.width = "0";
+	const aEls = sideBar.getElementsByTagName('a');
+	const pEls = sideBar.getElementsByTagName('p');
+	const elements = [...aEls, ...pEls];
+	elements.forEach(elem => {
+		elem.style.display = 'none';
+	});
+	document.querySelector('.profile').style.display = 'none';
+}
+
+// 
