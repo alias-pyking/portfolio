e6a33abd06123b7d5940f55be74f18312af2b52d
shubhamsks <shubhamkumar7051@gmail.com>
2020-09-19 14:29:22 +0530

    ui fixes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shubhamsks.pythonanywhere.com', 'localhost','127.0.0.1']
 
